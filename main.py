cb81066d38c51630f377c00a6a313612fd625114
Walker Laury <walkerclaury@protonmail.com>
2019-04-24 09:41:46 -0500

    Starting 5-9

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 #type//name/username/localhostport/database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:Copper14@localhost:3306/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:localhost@localhost:3306/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 
 #creates new database object
@@ -16,10 +16,10 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    
+    completed = db.Column(db.Boolean, default = False)
     def __init__(self, name):
         self.name = name
-    
+        self.completed = False
 
 
 
@@ -33,18 +33,20 @@ def index():
         db.session.commit()
         
 
-    tasks = Task.query.all()
-    return render_template('todos.html', title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html', title="Get It Done!",
+     tasks=tasks, completed_tasks = completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
     return redirect('/')
-
 
 
 
