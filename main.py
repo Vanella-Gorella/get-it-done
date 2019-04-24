a38838f2c08b2eb73fde657c5deb930712960e44
Walker Laury <walkerclaury@protonmail.com>
2019-04-24 15:22:42 -0500

    finished the prework

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 #type//name/username/localhostport/database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:localhost@localhost:3306/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
-
+app.secret_key = 'wakaflakaflame'
 #creates new database object
 db = SQLAlchemy(app)
 
@@ -17,24 +17,88 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean, default = False)
-    def __init__(self, name):
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __init__(self, name, owner):
         self.name = name
         self.completed = False
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login','register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
 
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash('Logged In')
+            print(session)
+            return redirect('/')
+        else:
+            flash('User Password incorrect, or user does not exist', 'error')
+            
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/')
+        else:
+            #Provide response
+            return '<h1>User already exists</h1>'
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/login')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        owner = User.query.filter_by(email=session['email']).first()
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
         
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    tasks = Task.query.filter_by(completed=False, owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed=True owner=owner).all()
     return render_template('todos.html', title="Get It Done!",
      tasks=tasks, completed_tasks = completed_tasks)
 
